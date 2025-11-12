46e078cc3121b0cac463956699585e4773d644df
Владислав Бевз <vladislav.bevz07@gmail.com>
2025-11-12 14:46:14 +0200

    Added all Repositories

---
+++ API/MusicPlaylistAPI/Repositories/FollowRepository.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using MusicPlaylistAPI.Models.Entity;
+
+namespace MusicPlaylistAPI.Repositories;
+
+public class FollowRepository : IFollowRepository
+{
+    private readonly IMongoCollection<Follow> _collection;
+
+    public FollowRepository() =>
+        _collection = MongoDBClient.Instance.GetCollection<Follow>("Follows");
+
+    public async Task CreateAsync(Follow follow) =>
+        await _collection.InsertOneAsync(follow);
+
+    public async Task<List<Follow>> GetAllAsync() =>
+        await (await _collection.FindAsync(_ => true)).ToListAsync();
+
+    public async Task<Follow> GetByIdAsync(string id) =>
+        await (await _collection.FindAsync(f => f.Id == id)).FirstOrDefaultAsync();
+
+    public async Task DeleteAsync(string id) =>
+        await _collection.DeleteOneAsync(f => f.Id == id);
+}
---
+++ API/MusicPlaylistAPI/Repositories/IFollowRepository.cs
@@ -0,0 +1,11 @@
+﻿using MusicPlaylistAPI.Models.Entity;
+
+namespace MusicPlaylistAPI.Repositories;
+
+public interface IFollowRepository
+{
+    Task CreateAsync(Follow follow);
+    Task<List<Follow>> GetAllAsync();
+    Task<Follow> GetByIdAsync(string id);
+    Task DeleteAsync(string id);
+}
