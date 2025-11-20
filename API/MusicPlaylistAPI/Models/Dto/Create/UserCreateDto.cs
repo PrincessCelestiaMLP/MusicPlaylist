3985565186a8f8ac5b4cdf63ab7a3b61d7e9bbaf
Владислав Бевз <vladislav.bevz07@gmail.com>
2025-11-20 13:33:44 +0200

    Commit

--- API/MusicPlaylistAPI/Models/Dto/Create/UserCreateDto.cs
+++ API/MusicPlaylistAPI/Models/Dto/Create/UserCreateDto.cs
@@ -2,7 +2,7 @@
 
 public class UserCreateDto
 {
-    public string Username;
-    public string Email;
-    public string Password;
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
