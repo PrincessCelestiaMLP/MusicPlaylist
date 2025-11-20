fca35e12075899daaacc00070e3afc4e60ee006c
Владислав Бевз <vladislav.bevz07@gmail.com>
2025-11-20 15:46:25 +0200

    User passworf

--- API/MusicPlaylistAPI/Models/Dto/Get/UserGetDto.cs
+++ API/MusicPlaylistAPI/Models/Dto/Get/UserGetDto.cs
@@ -5,5 +5,6 @@ public class UserGetDto
     public string Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
     public List<PlaylistGetDto> Playlists { get; set; }
 }
